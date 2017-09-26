69d13d71dccaa28fc131cb433fb372ddcb70b430
quyn <quy.nguyen@gbst.com>
2017-09-26 14:58:46 +1300

    Initial Commit for RFID reading using Phidget1023 reader

---
+++ src/com/qrolling/rfid/utils/RFIDMultiTagSimulator.java
@@ -0,0 +1,11 @@
+package com.qrolling.rfid.utils;
+
+/**
+ * @author Quy Nguyen (nguyenledinhquy@gmail.com) on 9/25/17
+ */
+public class RFIDMultiTagSimulator implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
