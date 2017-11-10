feb030cd530fa0f0047cc3acf7e4c002c4d218f9
quynguyenhcm <nguyenledinhquy@gmail.com>
2017-11-10 23:46:02 +1300

    Add Executable

---
+++ src/main/java/com/qrolling/rfid/utils/RSSSimulator.java
@@ -0,0 +1,10 @@
+package com.qrolling.rfid.utils;
+
+/**
+ * @author Quy Nguyen (nguyenledinhquy@gmail.com) on 9/25/17
+ */
+public class RSSSimulator {
+    public Double getRSS(){
+        return Math.random();
+    }
+}
